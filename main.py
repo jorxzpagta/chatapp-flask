351fec9901aa956a300a8f9c09df5a5936411fc4
jorxzpagta <jorexpagtalunan@yahoo.com>
2022-09-14 20:32:30 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, redirect, url_for
 from flask_socketio import SocketIO, send, emit
 
-
+# initializations
 app = Flask(__name__)
 app.config['SECRET'] = 'dev'
 socketio = SocketIO(app, cors_allowed_origins='*')
 
-
 # event handlers : What to do with the 'message'
+
+
 @socketio.on('message')
 def handle_message(message):
     print("Received message: " + message)
@@ -16,19 +17,22 @@ def handle_message(message):
 
 
 @socketio.on('sendMessage')
-def handle_my_custom_event(message):
-    print("Received message: " + message)
-    if message != "User Connected!":
-        emit(message, broadcast=True)
+def handle_my_custom_event(name, msg):
+    print("Received message: " + name + ": " + msg)
+    emit('appendMessage', (name, msg), broadcast=True)
 
 
 # Navigation
+@app.route('/chatapp', methods=['GET', 'POST'])
+def chatapp():
+    user = request.form.get('name')
+    return render_template('index.html', user=user)
+
 @app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 
 if __name__ == '__main__':
-    socketio.run(app, host="localhost") # Run in localhost
-
-
+    socketio.run(app, host="192.168.0.107")  # Run in localhost
--- static/index.js
+++ static/index.js
@@ -1,4 +1,4 @@
-var  socket = io.connect("http://localhost:5000");
+var  socket = io.connect("http://192.168.0.107:5000");
 
 function connect(){
 
@@ -13,36 +13,53 @@ function updateScrollbar(){
         messageBody.scrollTop = messageBody.scrollHeight - messageBody.clientHeight;
 }
 
+function filterMessage(name, msg){
+var $layout = $('<p></p>');
+var d = new Date();
+let hour = d.getHours();
+let minutes = d.getMinutes();
+let time = hour + ":" + minutes;
+if(name == $('#user').val()){
+ content = '<div class="media media-chat media-chat-reverse">' +
+'<div class="media-body"><p>'+ msg +'</p></div></div>';
+}
+else {
+ content = '<div class="media media-chat">' +
+'<div class="media-body"><p class="meta"><small>'+ name +'</small></p><p>'+ msg +'</p></div></div>';
+}
+
+return content
+}
+
 
 function updateMessageDisplay(){
 
-    socket.on('message', function(data){
-    $('#chat-content').append($('<p>').text(data));
+    socket.on('appendMessage', function(name, msg){
+    var $layout = $(filterMessage(name, msg));
+    //var layout = name + ": " + msg;
+    $('#chat-content').append($layout);
     updateScrollbar();
     });
-
-    socket.on((arg1) => {
-    $('#chat-content').append($('<p>').text(arg1)); // 1
-    updateScrollbar();
-    });
-
 }
+
+
 
 //jquery
 // Adds message in message box
 $(document).ready(function(){
     connect();
-
+    // Updates the message display to the latest message
+    updateScrollbar();
 // on Send Button click
     $("#send_Btn").on('click', function(){
-    socket.send($('#username').val() + ": " + $('#msg_Box').val());
+    socket.emit('sendMessage',$('#user').val(),$('#msg_Box').val());
     $('#msg_Box').val('');
     });
 
 // on Press Enter
     $("#msg_Box").on('keypress', function(event){
     if (event.key == "Enter") {
-    socket.emit('sendMessage', $('#username').val() + ": " + $('#msg_Box').val());
+    socket.emit('sendMessage',$('#user').val(),$('#msg_Box').val());
     $('#msg_Box').val('');
     }
     });
